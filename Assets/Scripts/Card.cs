96c4d01586de2dcec51cf9a345a6950bc6863014
eli <elijah.moody@outlook.com>
2026-03-05 14:19:10 -0500

    Draggable Script Functional

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -2,5 +2,12 @@ using UnityEngine;
 
 public class Card : MonoBehaviour
 {
-    
+    public bool placed = false;
+
+    public bool tryPlaceCard()
+    {
+        var transformPos = transform.position;
+        Physics2D.OverlapPoint(transformPos);
+        return true;
+    }
 }
---
+++ Assets/Scripts/Draggable.cs
@@ -0,0 +1,57 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Draggable : MonoBehaviour
+{
+    private Vector3 offSet = Vector3.zero;
+    private bool active = true;
+    private Card cardScript;
+
+    void Start()
+    {
+        var collider = gameObject.GetComponent<Collider2D>();
+        cardScript = gameObject.GetComponent<Card>();
+        if (collider == null)
+        {
+            Debug.LogError("Draggable object " + gameObject.name + " does not have a Collider component.");
+        }
+    }
+
+    void Update()
+    {
+        if (cardScript.placed)
+        {
+            active = false;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        if (!active)
+            return;
+        Debug.Log("Mouse down on " + gameObject.name);
+        var mouseScreenSpace = Input.mousePosition;
+        var mouseWorldPos = Camera.main.ScreenToWorldPoint(mouseScreenSpace);
+        offSet = gameObject.transform.position - mouseWorldPos;
+    }
+
+    void OnMouseDrag()
+    {
+        if (!active)
+            return;
+        var mouseScreenSpace = Input.mousePosition;
+        var mouseWorldPos = Camera.main.ScreenToWorldPoint(mouseScreenSpace);
+        gameObject.transform.position = mouseWorldPos + offSet;       
+    }
+
+    void onMouseUp()
+    {
+        if (!active)
+            return;
+        Debug.Log("Mouse up on " + gameObject.name);
+        if (cardScript.tryPlaceCard())
+        {
+            active = false;
+        }
+    }
+}
