459a5074bbf3ccda7128f56673e167d285384a02
eli <elijah.moody@outlook.com>
2026-03-17 10:00:33 -0400

    Card type flags, reference to surrounding cards

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,9 +1,49 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Card : MonoBehaviour
 {
     public bool placed = false;
     public CardManager cardManager;
+    [SerializeField]
+    private int deadliness = 0;
 
+    public Card nCard;
+    public Card sCard;
+    public Card eCard;
+    public Card wCard;
 
+    [Flags]
+    public enum CardType
+    {
+        None = 0,
+        Entrance = 1,
+        Treasure = 2,
+        Monster = 4,
+        Trap = 8,
+        Shrine = 16,
+        Alchemy = 32,
+        Hall = 64
+    }
+
+    public CardType types;
+
+    public void addDeadliness(int amount)
+    {
+        deadliness += amount;
+    }
+
+    /*public List<GameObject> GetTriggers()
+    {
+        List<GameObject> triggers = new List<GameObject>();
+        foreach (Transform child in transform)
+        {
+            if (child.CompareTag("tileDrop"))
+            {
+                triggers.Add(child.gameObject);
+            }
+        }
+        return triggers;
+    }*/
 }
--- Assets/Scripts/CardManager.cs
+++ Assets/Scripts/CardManager.cs
@@ -1,8 +1,21 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CardManager : MonoBehaviour
 {
+    private Card currentEntrace;
+    private List<Card> cards;
+
+    public int FewestRoomsFromEntrace(Card card)
+    {
+        if(card == currentEntrace || card == null)
+        {
+            return 0;
+        }
+        return 1;//temp
+    }
+
     public bool tryPlaceCard(Card card)
     {
         var cardPos = card.transform.position;
@@ -14,6 +27,21 @@ public class CardManager : MonoBehaviour
             {
                 card.placed = true;
                 card.transform.position = collider.transform.position;
+                switch (collider.name)
+                {
+                    case "NTtrig":
+                        card.nCard = collider.transform.parent.GetComponent<Card>();
+                        break;
+                    case "STtrig":
+                        card.sCard = collider.transform.parent.GetComponent<Card>();
+                        break;
+                    case "ETtrig":
+                        card.eCard = collider.transform.parent.GetComponent<Card>();
+                        break;
+                    case "WTtrig":
+                        card.wCard = collider.transform.parent.GetComponent<Card>();
+                        break;
+                }
                 Debug.Log("Placed card " + card.name + " on " + collider.gameObject.name);
                 return true;
             }
@@ -21,5 +49,10 @@ public class CardManager : MonoBehaviour
         Debug.Log("Could not place card " + card.name + " at position " + cardPos);
         //Debug.Log("Collider at position: " + (collider != null ? collider.gameObject.name : "None"));
         return false;
+    }
+
+    public void AddDeadlinessToCard(Card card, int amount)
+    {
+        card.addDeadliness(amount);
     }
 }
